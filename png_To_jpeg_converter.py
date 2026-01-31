b081471fac0bc90aa90eff981ac8116984d2ff02
JMG3000 <jakeincbusinesssolutions@gmail.com>
2026-01-30 18:08:32 -0600

    Added new variable names to reduce confusion. Changed the escape character to a forward slash. -jmg

--- png_To_jpeg_converter.py
+++ png_To_jpeg_converter.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python3
 # png_To_jpeg_converter.py
 # This script converts a png image file to a jpeg image file and
-# saves the converted image to a new file
+# saves the converted image to a new file.
 # Written by: JakeTheSnake(JMG3000)
 
 from PIL import Image
 import os
-import itertools
 
 
 def converter():
     stop = True
-    exit_word = ("stop","exit","quit","end")
     while stop:
         # Define the path to the desired folder
-        PICTURES_PATH = os.path.expanduser('~\Pictures\headshots')
+        pic_path = os.path.expanduser('~/Pictures/headshots')
 
         user_png_path = input("Please enter the path and name of the png you wish to convert: ")
 
         if user_png_path == "":
-            input_path = PICTURES_PATH + "\closer_headshot_blackSuitAndRedTie.png"
+            input_path = pic_path + "/closer_headshot_blackSuitAndRedTie.png"
 
-        elif exit_word.count(user_png_path):
-
-            stop = False
-            break
 
         else:
             input_path = user_png_path
@@ -32,11 +26,7 @@ def converter():
         user_jpeg_path = input("Please enter the path and name of the converted jpeg you wish to save: ")
 
         if user_jpeg_path == "":
-            output_path = PICTURES_PATH + "\closer_pro_headshot_high_quality.jpg"
-
-        elif exit_word.count(user_jpeg_path):
-            stop = False
-            break
+            output_path = pic_path + "/closer_pro_headshot_high_quality.jpg"
 
         else:
             output_path = user_jpeg_path
@@ -44,17 +34,18 @@ def converter():
 
         # attempt to open the file
         try:
-            img = Image.open(input_path)
+            img_data = Image.open(input_path)
             print("Opening {}".format(input_path))
 
         # catch an exception when the original file is not opened
         except Exception as e:
             print(f"Failed to open {input_path}. Error Code{e}")
+            break
 
 
         # attempt to convert the file after opening is successful
         try:
-            img = img.convert("RGB")
+            img_data = img_data.convert("RGB")
             print("Converting {}".format(input_path))
 
         # catch an exception when the original file is not converted
@@ -64,7 +55,7 @@ def converter():
 
         # attempt to save the file after convertion is successful
         try:
-            img.save(output_path, "JPEG", quality=95)
+            img_data.save(output_path, "JPEG", quality=95)
             print("Saved {}".format(output_path))
 
         # catch an exception when the converted file is not saved
